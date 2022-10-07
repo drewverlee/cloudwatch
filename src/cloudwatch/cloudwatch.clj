a5beb904895886d6c4a689b881ed04011bca9d1a
drew <drew.verlee@gmail.com>
2022-10-07 11:36:52 -0500

    init project setup

---
+++ src/cloudwatch/cloudwatch.clj
@@ -0,0 +1,12 @@
+(ns cloudwatch.cloudwatch
+  "FIXME: my new org.corfield.new/scratch project.")
+
+(defn exec
+  "Invoke me with clojure -X cloudwatch.cloudwatch/exec"
+  [opts]
+  (println "exec with" opts))
+
+(defn -main
+  "Invoke me with clojure -M -m cloudwatch.cloudwatch"
+  [& args]
+  (println "-main with" args))
